eb52c286defa0cab66c618392cc8b780be8d5fbf
Jan Phillip Kretzschmar <janphkre@gmx.de>
2019-09-29 19:38:45 +0200

    Documentation on BuildMonitorResult

--- plugin/src/main/java/de/janphkre/buildmonitor/BuildMonitorResult.kt
+++ plugin/src/main/java/de/janphkre/buildmonitor/BuildMonitorResult.kt
@@ -2,11 +2,18 @@ package de.janphkre.buildmonitor
 
 import de.janphkre.buildmonitor.util.Hashable
 import de.janphkre.buildmonitor.util.Hashing
-import java.util.*
 
+/**
+ * Complete result of the monitoring, ready to be sent off / shared.
+ * It can be serialized by calling toString() on it which will create a JSON string of it.
+ * The resulting string will not contain values that were already contained the last time a build
+ * was monitored, effectively only showing differences during builds.
+ * This diff is achieved by creating and storing hash codes for the individual fields of this class.
+ * Hash codes are maintained by the de.janphkre.buildmonitor.util.Hashing object.
+ */
 class BuildMonitorResult {
 
-    val environment = TreeMap<String, String>()
+    val environment = HashMap<String, String>()
 
     private fun StringBuilder.writeWithHash(hashCode: Int, hashKey: Hashable, serializer: StringBuilder.() -> Unit): StringBuilder {
         this.append("\"hashCode\":$hashCode")
@@ -19,6 +26,9 @@ class BuildMonitorResult {
         return this
     }
 
+    /**
+     * Serializes this result into a JSON string.
+     */
     override fun toString(): String {
         return StringBuilder()
             .append("{\"environment\":{")
